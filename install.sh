069565ea409df1eacdc67ccc126ad545527b9777
The-Kid-Gid <gideonshaked@gmail.com>
2021-01-15 09:46:48 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Wscli install script
+#
+
+main() {
+    check_java
+
+    git clone https://github.com/The-Kid-Gid/wscli "$HOME/.local/bin/wscli" > /dev/null
+
+    add_to_bashrc "# !!! This block added by wscli install script !!!"
+    add_to_bashrc 'alias wscli="java $HOME/.local/bin/wscli/src/wscli.java'
+    add_to_bashrc "# !!! End block !!!"    
+}
+
+check_java() {
+    if ! command -v java &> /dev/null
+    then
+        echo "Java not found. Exiting..."
+        exit 1
+    fi
+}
+
+add_to_bashrc() {
+    echo "$1" >> "$HOME/.bashrc"
+}
+
+main
